954a7db8ce8949c731e4837c9f0c2d835462d922
26021auca <manzidelphin2@gmail.com>
2026-02-18 18:42:39 +0200

    Docs: Add comments to Customer model class

--- app/src/main/java/com/example/quiz_four/Customer.java
+++ app/src/main/java/com/example/quiz_four/Customer.java
@@ -2,6 +2,9 @@ package com.example.quiz_four;
 
 import java.util.Date;
 
+/**
+ * Model class representing a Customer.
+ */
 public class Customer {
     private int customerID;
     private String fullNames;
@@ -10,62 +13,65 @@ public class Customer {
     private String nationality;
     private String customerNID;
 
-    public Customer() {
-    }
+    // Default constructor
+    public Customer() {}
 
-    public Customer(int customerID, String fullNames, String gender, Date birthDate, String nationality, String customerNID) {
-        this.customerID = customerID;
-        this.fullNames = fullNames;
-        this.gender = gender;
-        this.birthDate = birthDate;
-        this.nationality = nationality;
-        this.customerNID = customerNID;
-    }
-
+    // Getter for customerID
     public int getCustomerID() {
         return customerID;
     }
 
+    // Setter for customerID
     public void setCustomerID(int customerID) {
         this.customerID = customerID;
     }
 
+    // Getter for fullNames
     public String getFullNames() {
         return fullNames;
     }
 
+    // Setter for fullNames
     public void setFullNames(String fullNames) {
         this.fullNames = fullNames;
     }
 
+    // Getter for gender
     public String getGender() {
         return gender;
     }
 
+    // Setter for gender
     public void setGender(String gender) {
         this.gender = gender;
     }
 
+    // Getter for birthDate
     public Date getBirthDate() {
         return birthDate;
     }
 
+    // Setter for birthDate
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
     }
 
+    // Getter for nationality
     public String getNationality() {
         return nationality;
     }
 
+    // Setter for nationality
     public void setNationality(String nationality) {
         this.nationality = nationality;
     }
 
+    // Getter for customerNID
     public String getCustomerNID() {
         return customerNID;
     }
 
+    // Setter for customerNID
     public void setCustomerNID(String customerNID) {
         this.customerNID = customerNID;
     }
